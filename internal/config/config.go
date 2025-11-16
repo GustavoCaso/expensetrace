f1c2017f596dbaca1fd176ed5e43c483cd490121
Gustavo Caso <gustavocaso@gmail.com>
2025-11-16 19:49:40 +0100

    remove configuration file && bump version

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,74 +1,53 @@
 package config
 
 import (
-	"errors"
 	"os"
-
-	"gopkg.in/yaml.v3"
 
 	"github.com/GustavoCaso/expensetrace/internal/logger"
 )
 
 type Config struct {
-	DB     string        `yaml:"db"`
-	Logger logger.Config `yaml:"logger"`
+	DBFile string
+	Logger logger.Config
 }
+
+const (
+	defaultDBFile    = "expensetrace.db"
+	defaultLogLevel  = logger.LevelInfo
+	defaultLogFormat = logger.FormatText
+	defaultLogOutput = "stdout"
+)
 
 func (c *Config) parseEnv() {
-	if c.DB == "" {
-		if db := os.Getenv("EXPENSETRACE_DB"); db != "" {
-			c.DB = db
-		} else {
-			c.DB = "expensetrace.db"
-		}
+	if db := os.Getenv("EXPENSETRACE_DB"); db != "" {
+		c.DBFile = db
+	} else {
+		c.DBFile = defaultDBFile
 	}
 
-	if c.Logger.Level == "" {
-		if level := os.Getenv("EXPENSETRACE_LOG_LEVEL"); level != "" {
-			c.Logger.Level = logger.Level(level)
-		} else {
-			c.Logger.Level = logger.LevelInfo
-		}
+	if level := os.Getenv("EXPENSETRACE_LOG_LEVEL"); level != "" {
+		c.Logger.Level = logger.Level(level)
+	} else {
+		c.Logger.Level = defaultLogLevel
 	}
 
-	if c.Logger.Format == "" {
-		if format := os.Getenv("EXPENSETRACE_LOG_FORMAT"); format != "" {
-			c.Logger.Format = logger.Format(format)
-		} else {
-			c.Logger.Format = logger.FormatText
-		}
+	if format := os.Getenv("EXPENSETRACE_LOG_FORMAT"); format != "" {
+		c.Logger.Format = logger.Format(format)
+	} else {
+		c.Logger.Format = defaultLogFormat
 	}
 
-	if c.Logger.Output == "" {
-		if output := os.Getenv("EXPENSETRACE_LOG_OUTPUT"); output != "" {
-			c.Logger.Output = output
-		} else {
-			c.Logger.Output = "stdout"
-		}
+	if output := os.Getenv("EXPENSETRACE_LOG_OUTPUT"); output != "" {
+		c.Logger.Output = output
+	} else {
+		c.Logger.Output = defaultLogOutput
 	}
 }
 
-func Parse(file string) (*Config, error) {
+func Parse() *Config {
 	conf := &Config{}
-
-	_, statErr := os.Stat(file)
-	if statErr != nil && !errors.Is(statErr, os.ErrNotExist) {
-		return nil, statErr
-	}
-
-	if statErr == nil {
-		bytes, err := os.ReadFile(file)
-		if err != nil {
-			return nil, err
-		}
-
-		err = yaml.Unmarshal(bytes, conf)
-		if err != nil {
-			return nil, err
-		}
-	}
 
 	conf.parseEnv()
 
-	return conf, nil
+	return conf
 }
